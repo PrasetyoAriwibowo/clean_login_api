bd1d99506cf2c1cb074bdd1da680aa63edec48df
prasetyo <po.prasetyo99@gmail.com>
2022-12-13 17:33:23 +0700

    login_api

--- lib/presentation/login/screens/login.screen.dart
+++ lib/presentation/login/screens/login.screen.dart
@@ -13,116 +13,123 @@ class LoginScreen extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            color: Coolors.purplish,
-          ),
-          Container(
-            width: context.screenWidth,
-            height: context.percentHeight * 80,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(60),
-                  bottomRight: Radius.circular(60),
-                )),
-            child: VStack([
+      body: Container(
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                color: Coolors.purplish,
+              ),
               Container(
                 width: context.screenWidth,
-                height: 160,
-                child: Image.asset("assets/images/login.png"),
-              ),
-              TextField(
-                controller: controller.usernameController,
-                autofocus: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15)),
-                  labelText: "Username",
-                  contentPadding: Vx.m2,
-                  hintText: "Input your username",
-                  prefixIcon: Icon(
-                    Icons.people,
+                height: context.percentHeight * 70,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(60),
+                      bottomRight: Radius.circular(60),
+                    )),
+                child: VStack([
+                  Container(
+                    margin: EdgeInsets.only(top: 50),
+                    width: context.screenWidth,
+                    height: 160,
+                    child: Image.asset("assets/images/login.png"),
                   ),
-                ),
-              ).p16(),
-              Container(
-                  child: Obx(() => (TextField(
-                        controller: controller.passwordController,
-                        autofocus: true,
-                        obscureText: controller.isHidePassword.value,
-                        decoration: InputDecoration(
-                            contentPadding: Vx.m2,
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(15)),
-                            labelText: "Input your password",
-                            prefixIcon: Icon(
-                              Icons.lock,
-                            ),
-                            suffixIcon: IconButton(
-                                onPressed: (() {
-                                  controller.isHidePassword.value =
-                                      !controller.isHidePassword.value;
-                                }),
-                                icon: controller.isHidePassword.value
-                                    ? Icon(Icons.visibility_off)
-                                    : Icon(Icons.visibility))),
-                      ).p16()))),
-              "Forgot password?"
-                  .text
-                  .sm
-                  .caption(context)
-                  .make()
-                  .objectCenterRight()
-                  .p16(),
-              ElevatedButton(
-                  onPressed: () async {
-                    await controller.login();
-                  },
-                  child: "Log In".text.bold.make(),
-                  style: ElevatedButton.styleFrom(
-                    shape: StadiumBorder(),
-                    primary: Coolors.purplish,
-                    textStyle: TextStyle(color: Colors.white),
-                  )).wh(context.screenWidth, 48).p16()
-            ]),
-          ),
-          Positioned(
-            child: VStack(
-              [
-                HStack(
+                  TextField(
+                    controller: controller.usernameController,
+                    autofocus: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15)),
+                      labelText: "Username",
+                      contentPadding: Vx.m2,
+                      hintText: "Input your username",
+                      prefixIcon: Icon(
+                        Icons.people,
+                      ),
+                    ),
+                  ).p16(),
+                  Container(
+                      child: Obx(() => (TextField(
+                            controller: controller.passwordController,
+                            autofocus: true,
+                            obscureText: controller.isHidePassword.value,
+                            decoration: InputDecoration(
+                                contentPadding: Vx.m2,
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(15)),
+                                labelText: "Input your password",
+                                prefixIcon: Icon(
+                                  Icons.lock,
+                                ),
+                                suffixIcon: IconButton(
+                                    onPressed: (() {
+                                      controller.isHidePassword.value =
+                                          !controller.isHidePassword.value;
+                                    }),
+                                    icon: controller.isHidePassword.value
+                                        ? Icon(Icons.visibility_off)
+                                        : Icon(Icons.visibility))),
+                          ).p16()))),
+                  "Forgot password?"
+                      .text
+                      .sm
+                      .caption(context)
+                      .make()
+                      .objectCenterRight()
+                      .p16(),
+                  ElevatedButton(
+                      onPressed: () async {
+                        await controller.login();
+                      },
+                      child: "Log In".text.bold.make(),
+                      style: ElevatedButton.styleFrom(
+                        shape: StadiumBorder(),
+                        primary: Coolors.purplish,
+                        textStyle: TextStyle(color: Colors.white),
+                      )).wh(context.screenWidth, 48).p16()
+                ]),
+              ),
+              Positioned(
+                child: VStack(
                   [
-                    SvgPicture.asset('assets/fb.svg'),
-                    SvgPicture.asset('assets/google.svg'),
-                    SvgPicture.asset('assets/twitter.svg'),
+                    HStack(
+                      [
+                        SvgPicture.asset('assets/fb.svg'),
+                        SvgPicture.asset('assets/google.svg'),
+                        SvgPicture.asset('assets/twitter.svg'),
+                      ],
+                      alignment: MainAxisAlignment.center,
+                    ).wFull(context),
+                    16.heightBox,
+                    Row(
+                      children: [
+                        Container(
+                          child: Text("Don't have an account? ",
+                              style: TextStyle(color: Colors.white)),
+                        ),
+                        InkWell(
+                            child: Text(
+                              'Sign Up here!',
+                              style: TextStyle(color: Coolors.orangish),
+                            ),
+                            onTap: () {
+                              Get.toNamed(Routes.REGISTER);
+                            }),
+                      ],
+                    )
                   ],
                   alignment: MainAxisAlignment.center,
-                ).wFull(context),
-                16.heightBox,
-                Row(
-                  children: [
-                    Container(
-                      child: Text("Don't have an account? ",
-                          style: TextStyle(color: Colors.white)),
-                    ),
-                    InkWell(
-                        child: Text(
-                          'Sign Up here!',
-                          style: TextStyle(color: Coolors.orangish),
-                        ),
-                        onTap: () {
-                          Get.toNamed(Routes.REGISTER);
-                        }),
-                  ],
-                )
-              ],
-              alignment: MainAxisAlignment.center,
-              crossAlignment: CrossAxisAlignment.center,
-            ),
-            bottom: 24,
+                  crossAlignment: CrossAxisAlignment.center,
+                ),
+                bottom: 24,
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
